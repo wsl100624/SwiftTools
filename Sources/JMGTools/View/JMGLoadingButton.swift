44fb2aff652c2bd567c49a8bfe36a8c01792655a
Will Wang <willwang4859@gmail.com>
2021-03-18 14:13:29 -0500

    added loading button

---
+++ Sources/JMGTools/View/JMGLoadingButton.swift
@@ -0,0 +1,41 @@
+//
+//  JMGLoadingButton.swift
+//  
+//
+//  Created by Will Wang on 3/18/21.
+//
+
+import UIKit
+
+open class JMGLoadingButton: UIButton {
+    
+    open var indicatorColor: UIColor = .white
+    open var indicatorStyle: UIActivityIndicatorView.Style = .medium
+    
+    private var originalTitleColor: UIColor = .white
+    
+    private lazy var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: indicatorStyle)
+        indicator.color = indicatorColor
+        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+    
+    open func showLoading() {
+        originalTitleColor = self.currentTitleColor
+        setTitleColor(UIColor.clear, for: .normal)
+        
+        self.addSubview(activityIndicator)
+        activityIndicator.centerInSuperview()
+        activityIndicator.startAnimating()
+    }
+    
+    open func hideLoading() {
+        DispatchQueue.main.async(execute: {
+            self.setTitleColor(originalTitleColor, for: .normal)
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.removeFromSuperview()
+        })
+    }
+}
+
